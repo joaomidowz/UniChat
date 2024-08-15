3d815e508a6e8dcfee4ef674d906c5783639fcaa
João Midowz <jgcustodio2005@gmail.com>
2024-08-15 14:30:00 -0300

    Adiciona .gitignore para ignorar node_modules, .wwebjs_cache e package-lock.json

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+
+client.initialize();
+
+
+// Listening to all incoming messages
+
+
